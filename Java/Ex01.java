9f767137f11afeda7dd6ebde6441206577db6822
Debora Campos <105235151+dcmicas@users.noreply.github.com>
2022-06-08 08:51:08 -0300

    Add files via upload
    First exercises with Java

---
+++ Java/Ex01.java
@@ -0,0 +1,16 @@
+/*  Create variables for the fields described below between <> and print the following message:
+I <name>, living at <address>, confirm that I received the salary of <salary> on the date <date> */
+
+
+public class Ex01 {
+    public static void main(String[] args) {
+        String name = "Debora Campos";
+        String address = "Crazy Crazy Street #0";
+        double salary = 1234.5;
+        String salaryReceiptDate = "07/06/2022";
+
+      System.out.println("I " +(name)+ ",living at " +(address)+ ", confirm that I received the salary of "
+                +(salary)+ " on the date " +(salaryReceiptDate));
+
+    }}
+
