b29c97ee437b552fc0b8cc6ef9e86316074a0173
dcmicas <dcmicas@gmail.com>
2022-06-19 22:35:24 -0300

    Angular
    Angular study

--- Angular/componentsStudy/src/app/app.module.ts
+++ Angular/componentsStudy/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { WarningAlertComponent } from './warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WarningAlertComponent,
-    SuccessAlertComponent
+    SuccessAlertComponent,
+    ServersComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule
